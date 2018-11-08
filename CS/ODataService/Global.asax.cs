059f0d00b91adbc0353babef01b1ddfe16bd323d
karpenko.maksim <maksim.karpenko@devexpress.com>
2018-11-08 11:12:28 +0300

    Change persistent types detection in CustomBodyModelValidator

--- CS/ODataService/Global.asax.cs
+++ CS/ODataService/Global.asax.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Collections.Concurrent;
 using System.Web.Http;
 using System.Web.Http.Validation;
-using System.Web.ModelBinding;
-using System.Web.Routing;
 using DevExpress.Xpo;
 using DevExpress.Xpo.DB;
 using ODataService.Helpers;
-using WebApplication1.Models;
 
 namespace WebApplication1
 {
@@ -22,12 +17,9 @@ namespace WebApplication1
         }
 
         public class CustomBodyModelValidator : DefaultBodyModelValidator {
-            readonly HashSet<Type> persistentTypes;
-            public CustomBodyModelValidator() {
-                persistentTypes = new HashSet<Type>(ConnectionHelper.GetPersistentTypes());
-            }
+            readonly ConcurrentDictionary<Type, bool> persistentTypes = new ConcurrentDictionary<Type, bool>();
             public override bool ShouldValidateType(Type type) {
-                return !persistentTypes.Contains(type);
+                return persistentTypes.GetOrAdd(type, t => !typeof(IXPSimpleObject).IsAssignableFrom(t));
             }
         }
     }
